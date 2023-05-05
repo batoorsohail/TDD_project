0f1edb1217e8ce98e3d6e2cb4b179cfb1a04ad3f
unknown <sohailshahidi1435@gmail.com>
2023-05-05 10:43:03 +0430

    Add fizzbuzz Method

--- solver.rb
+++ solver.rb
@@ -1,3 +1,13 @@
 class Solver
-  
+  def fizzbuzz(num)
+    if (num % 15).zero?
+      'fizzbuzz'
+    elsif (num % 5).zero?
+      'buzz'
+    elsif (num % 3).zero?
+      'fizz'
+    else
+      num.to_s
+    end
+  end
 end
